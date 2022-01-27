5ed4c64e31bbcef8c033f346704cdcf684ebbf1d
ruz-gul <amin76heidarzadeh@gmail.com>
2022-01-27 10:42:07 +0330

    Test second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & Git
+          Test Github Desktop
         </a>
       </header>
     </div>
